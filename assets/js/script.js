8b2deeb6d3b4ac6839a5b436b9a64d5db2faeab5
Florin Pinta <xttrust@live.com>
2023-11-19 19:44:54 +0000

    removed extra comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const btnStart = document.getElementById("start");
+const localStorageData = getData();
 
 // Wait for the document to be loaded
 document.addEventListener("DOMContentLoaded", function () {
@@ -23,11 +24,11 @@ function startGame() {
     if (!checkUsername(username)) {
         return false;
     }
-    // Username was entered - Start the game
     hideStartGameWrapper(); // Hide the startGameWrapper
     // Start the game
-    showGame();
-    showGameContent(username, difficulty);
+    showGame(); // Display the game wrapper
+    fetchData(difficulty); // Fetch the data from api based on difficulty
+    showGameContent(username, difficulty); // Display the game content
 
 }
 
@@ -79,12 +80,10 @@ function showGameContent(username, difficulty) {
     gameContentWrapper.appendChild(gameWelcomeMessage);
 
     // Set the data to local strage
-    fetchData(difficulty);
 
-    // Get the data from local storage
-    let data = getData();
 
-    console.log(getFinalData(data[0]));
+
+    console.log(getFinalData(localStorageData[0]));
 }
 
 
@@ -98,10 +97,8 @@ function getFinalData(array) {
     // Copy the incorrect_answers array to avoid modifying the original
     let incorrectAnswersCopy = array.incorrect_answers.slice();
 
-    // Push the correct answer to the copied array
     incorrectAnswersCopy.push(array.correct_answer);
 
-    // Shuffle the combined array
     let shuffledAnswers = shuffleArray(incorrectAnswersCopy);
 
     let result = {
@@ -137,20 +134,15 @@ function shuffleArray(array) {
  * @returns {Promise<void>} A Promise that resolves when the data is fetched and stored.
  */
 function fetchData(difficulty) {
-    // Construct the API URL based on the provided difficulty
     let apiUrl = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=' + difficulty;
 
-    // Fetch data from the API, handle the response, and store in local storage
     return fetch(apiUrl)
         .then(handleResponse) // Handle the response (check for errors and parse as JSON)
         .then(data => {
-            // Remove existing data from local storage
             localStorage.removeItem('quizData');
-            // Store new data in local storage after converting to JSON string
             localStorage.setItem('quizData', JSON.stringify(data.results));
         })
         .catch(error => {
-            // Log an error message if there's an issue fetching data
             console.error('Error fetching data:', error);
         });
 }
@@ -162,7 +154,6 @@ function fetchData(difficulty) {
  * @returns {Promise<Object>} A Promise that resolves to the parsed JSON data.
  */
 function handleResponse(response) {
-    // Check if the response is okay, throw an error if not
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
@@ -174,9 +165,7 @@ function handleResponse(response) {
  * @returns {Array} The parsed data or null if no data is found.
  */
 function getData() {
-    // Retrieve the stored data from local storage
     let storedData = localStorage.getItem('quizData');
-
     return JSON.parse(storedData);
 }
 
